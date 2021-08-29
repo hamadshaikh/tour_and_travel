b3d6abea596012483b7f9095064f033fb363d309
HAMMAD AHMED SHIAKH <hammad2009f@aptechsite.net>
2021-08-29 04:56:08 -0700

    navbar cmp

--- main.js
+++ main.js
@@ -3,12 +3,26 @@ let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
- 
+let menu = document.querySelector('#menu_bar');
+let navbar = document.querySelector('.navbar');
+
+
+
+
+
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
-
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
 }
+
+
+menu.addEventListener('click',() =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
 
 searchBtn.addEventListener('click',() =>{
     searchBtn.classList.toggle('fa-times');
